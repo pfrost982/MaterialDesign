bac6bc1fff225a683885f29d656007c43013b45c
pfrost982 <power982@gmail.com>
2021-11-21 20:23:22 +0700

    увеличение снимка земли

--- app/src/main/java/geekbarains/material/ui/earth/EarthFragment.kt
+++ app/src/main/java/geekbarains/material/ui/earth/EarthFragment.kt
@@ -2,11 +2,18 @@ package geekbarains.material.ui.earth
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import coil.api.load
 import geekbarains.material.BuildConfig
 import geekbarains.material.R
 import kotlinx.android.synthetic.main.fragment_earth.*
+
+private var isExpanded = false
 
 class EarthFragment : Fragment() {
 
@@ -25,6 +32,22 @@ class EarthFragment : Fragment() {
             placeholder(R.drawable.ic_no_photo_vector)
         }
 
-    }
+        image_view.setOnClickListener {
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(
+                container, TransitionSet()
+                    .addTransition(ChangeBounds())
+                    .addTransition(ChangeImageTransform())
+            )
+            val params: ViewGroup.LayoutParams = image_view.layoutParams
+            params.height =
+                if (isExpanded) ViewGroup.LayoutParams.MATCH_PARENT else
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            image_view.layoutParams = params
+            image_view.scaleType =
+                if (isExpanded) ImageView.ScaleType.CENTER_CROP else
+                    ImageView.ScaleType.FIT_CENTER
+        }
 
+    }
 }
